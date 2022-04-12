84d6d9da06bb1090fd42a8623af005147ddd638f
hgssnk <hgssnk@gmail.com>
2022-04-12 23:44:30 +0900

    first

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,15 @@
+# Modules
+Import-Module PSReadLine
+Import-Module oh-my-posh
+Import-Module -Name Terminal-Icons
+Set-PSReadlineOption -EditMode Emacs
+Set-PSReadLineKeyHandler -Key Ctrl+d -Function DeleteChar
+
+# Themes
+Set-PoshPrompt -Theme night-owl
+
+# Alias
+Set-Alias ll ls
+Set-Alias vim nvim
+
+
